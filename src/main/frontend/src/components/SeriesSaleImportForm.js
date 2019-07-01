42da63c306c65fdaac4b45c6e0469c7aefd5b0db
Slava Semushin <slava.semushin@gmail.com>
2019-07-01 22:49:28 +0100

    fix TODO: add translations
    [skip ci]

--- src/main/frontend/src/components/SeriesSaleImportForm.js
+++ src/main/frontend/src/components/SeriesSaleImportForm.js
@@ -4,24 +4,22 @@
 //
 
 /* TODO: remove id attributes */
-const SeriesSaleImportForm = () => {
+const SeriesSaleImportForm = (props) => {
 	return (
 		<div className="row">
 			<div className="col-sm-12">
 				<div className="row">
 					<div className="col-sm-12">
-						{/* TODO: add translation: #{t_import_info_who_selling_series} */}
 						<h5>
-							Import info about selling this series
+							{ props.l10n['t_import_info_who_selling_series'] || 'Import info about selling this series' }
 						</h5>
 					</div>
 				</div>
 				<div className="row">
-					{/* TODO: add translation: #{t_could_not_import_info} */}
 					{/* TODO: add logic for hiding/showing this element */}
 					<div id="import-series-sale-failed-msg"
 						className="alert alert-danger text-center col-sm-8 col-sm-offset-2 hidden">
-						Could not import information from this page
+						{ props.l10n['t_could_not_import_info'] || 'Could not import information from this page' }
 					</div>
 				</div>
 				<div className="row">
@@ -30,9 +28,8 @@ const SeriesSaleImportForm = () => {
 							
 							<div className="form-group form-group-sm">
 								{/* TODO: deal with label for+id */}
-								{/* TODO: add translation: #{t_url} */}
 								<label htmlFor="series-sale-url" className="control-label col-sm-3">
-									URL
+									{ props.l10n['t_url'] || 'URL' }
 									<span className="required_field"> *</span>
 								</label>
 								<div className="col-sm-6">
@@ -44,9 +41,8 @@ const SeriesSaleImportForm = () => {
 							
 							<div className="form-group form-group-sm">
 								<div className="col-sm-offset-3 col-sm-4">
-									{/* TODO: add translation: #{t_import_info} */}
 									<button id="series-sale-submit-btn" type="submit" className="btn btn-primary">
-										Import info
+										{ props.l10n['t_import_info'] || 'Import info' }
 									</button>
 								</div>
 							</div>
@@ -57,5 +53,10 @@ const SeriesSaleImportForm = () => {
 			</div>
 		</div>
 	)
+}
+
+// required for prototype
+SeriesSaleImportForm.defaultProps = {
+	'l10n': {}
 }
 
